7034ce65a163b88010102c62f7158a3674c1b78f
Yuliia-Gorgodze <taragorgodze@gmail.com>
2021-07-08 09:42:20 +0300

    с

--- src/components/modalTransaction.js
+++ src/components/modalTransaction.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { FormControl, Input, FormHelperText, Modal } from '@material-ui/core';
+import {
+  FormControl,
+  Input,
+  FormHelperText,
+  Modal,
+  Select,
+  OutlinedInput,
+  MenuItem,
+} from '@material-ui/core';
 import { useSelector, useDispatch } from 'react-redux';
 import { modalTrancactionIsOpen } from '../redux/modaltransaction/modalTransactionOperations';
 import { IsModalTrasaction } from '../redux/modaltransaction/modalTransactionSelector';
@@ -36,7 +44,23 @@ export default function ModalAddTransaction() {
             </ul>
           </label>
         </div>
-
+        <Select
+          className={style.catrgoryMenu}
+          value="yulka"
+          input={<OutlinedInput name="category" id="menuCategory" />}
+        >
+          <option value="none" placeholder="Выберите категорию" disabled>
+            Выберите категорию
+          </option>
+          <MenuItem value="Основной">Основной</MenuItem>
+          <MenuItem value="Еда">Еда</MenuItem>
+          <MenuItem value="Авто">Авто</MenuItem>
+          <MenuItem value="Развитие">Развитие</MenuItem>
+          <MenuItem value="Дети">Дети</MenuItem>
+          <MenuItem value="Дом">Дом</MenuItem>
+          <MenuItem value="Образование">Образование</MenuItem>
+          <MenuItem value="Остальное">Остальное</MenuItem>
+        </Select>
         <Input id="my-input" aria-describedby="my-helper-text" />
         <FormHelperText id="my-helper-text">Коментарий</FormHelperText>
         <button type="submit" className={`${style.button} ${style.buttonAdd}`}>
