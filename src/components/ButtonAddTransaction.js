817913cbaec61e65c9a588341ec1daae493888c5
Yuliia-Gorgodze <taragorgodze@gmail.com>
2021-07-09 16:32:32 +0300

    add redax addNewTransaction and style form modalTransaction

--- src/components/ButtonAddTransaction.js
+++ src/components/ButtonAddTransaction.js
@@ -1,12 +1,11 @@
 import { useDispatch } from 'react-redux';
 import styles from './componentsCSS/ButtonAddTransaction.module.css';
 import { modalTrancactionIsOpen } from '../redux/modaltransaction/modalTransactionOperations';
-import chacked from './modalTransactionFunction';
+
 export default function ButtonAddTransaction() {
   const dispatch = useDispatch();
   const closeModal = e => {
     dispatch(modalTrancactionIsOpen(true));
-    chacked();
   };
   return (
     <button
--- src/redux/modaltransaction/modalTransactionReducer.js
+++ src/redux/modaltransaction/modalTransactionReducer.js
@@ -1,6 +1,10 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
-import { MODAL_IS_OPEN, ADD_NEW_TRANSACTION } from './modalTransactionActions';
+import {
+  MODAL_IS_OPEN,
+  ADD_NEW_TRANSACTION_SUCCES,
+  ADD_NEW_TRANSACTION_ERROR,
+} from './modalTransactionActions';
 
 const isModalTransaction = createReducer(false, {
   [MODAL_IS_OPEN]: (_, { payload }) => payload,
@@ -8,10 +12,14 @@ const isModalTransaction = createReducer(false, {
 const addNewTransaction = createReducer(
   {},
   {
-    [ADD_NEW_TRANSACTION]: (_, { payload }) => payload,
+    [ADD_NEW_TRANSACTION_SUCCES]: (_, { payload }) => payload,
   },
 );
+const error = createReducer(null, {
+  [ADD_NEW_TRANSACTION_ERROR]: (_, { payload }) => payload,
+});
 export const trasaction = combineReducers({
   isModalTransaction,
   addNewTransaction,
+  error,
 });
