c088c14b8f650abeae0a53cd03aaeede2ea73ce8
Yuliia-Gorgodze <taragorgodze@gmail.com>
2021-07-09 21:34:57 +0300

    add statisticPage

--- src/components/Statistic.js
+++ src/components/Statistic.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import style from './componentsCSS/Statistic.module.css';
+import Table from './Table';
+import Diagram from './Diagram';
 function Statistic() {
   return (
     <>
-      <div>
-        <h1>THIS IS STATISTIC PAGE</h1>
+      <h1 className={style.tittle}>Статистика</h1>
+      <div className={style.staticticContainer}>
+        <div className={style.diagram}>
+          <Diagram />
+        </div>
+        <div className={style.table}>
+          <Table />
+          <ul>
+            <li>
+              Расходы: <span>0</span>
+            </li>
+            <li>
+              Доходы: <span>100000000000$</span>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
