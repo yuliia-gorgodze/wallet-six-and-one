53cee5059b190d09fd34e3299b12a84d0267b5f5
m6xcom <65496114+m6xcom@users.noreply.github.com>
2021-07-13 22:29:58 +0300

    Adds styles on stats page

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,6 +7,8 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import style from './componentsCSS/Table.module.css';
+
 const useStyles = makeStyles({
   table: {
     minWidth: 250,
@@ -29,20 +31,20 @@ export default function DenseTable() {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.root}>
+    <Paper className={`${classes.root} ${style.table__container}`}>
       <TableContainer component={Paper}>
         <Table
-          className={classes.table}
+          className={`${classes.table} ${style.table}`}
           size="small"
           aria-label="a dense table"
         >
           <TableHead>
-            <TableRow>
+            <TableRow className={style.table__header}>
               <TableCell>Категория</TableCell>
               <TableCell align="right">Сумма</TableCell>
             </TableRow>
           </TableHead>
-          <TableBody>
+          <TableBody className={style.table__body} id="table__body">
             {rows.map(row => (
               <TableRow key={row.name}>
                 <TableCell component="th" scope="row">
