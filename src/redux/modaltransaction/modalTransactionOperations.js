c6c5e9b7b0c3958783781c250e2da114bf378225
Viktor-sun <bobick.zdoh@gmail.com>
2021-07-16 20:03:34 +0300

    add update table online

--- src/redux/modaltransaction/modalTransactionOperations.js
+++ src/redux/modaltransaction/modalTransactionOperations.js
@@ -1,4 +1,3 @@
-import notification from '../../helpers/react-toastify';
 import axios from 'axios';
 import {
   MODAL_IS_OPEN,
@@ -6,6 +5,8 @@ import {
   ADD_NEW_TRANSACTION_REQUEST,
   ADD_NEW_TRANSACTION_ERROR,
 } from './modalTransactionActions';
+import transactionOperations from '../transactions/transactionOperations';
+import notification from '../../helpers/react-toastify';
 
 export const modalTrancactionIsOpen = modalState => async dispatch => {
   dispatch(MODAL_IS_OPEN(modalState));
@@ -25,10 +26,12 @@ export const addTrancaction = transaction => async dispatch => {
       .post(`${axios.defaults.baseURL}/transactions`, result)
       .then(data => {
         dispatch(ADD_NEW_TRANSACTION_SUCCES(data.data.data.transaction));
-        console.log('Успешно добавлена транзакция');
+        //update table online
+        dispatch(transactionOperations.fetchTransactions());
+        notification.sucess('Транзакция успешно добавлена');
       })
       .catch(er => console.log(er));
-    console.log(`${axios.defaults.baseURL}/transactions`);
+    // console.log(`${axios.defaults.baseURL}/transactions`);
   } catch (error) {
     console.log(error);
     dispatch(ADD_NEW_TRANSACTION_ERROR('Не удалось добавить'));
